28f129d5ced7324242d3bbe92412d88a45117e7b
DharaniArumugam <59991327+Dharani-Arumugam@users.noreply.github.com>
2026-02-11 11:18:52 -0600

    upda

--- jobs/03_gold_kpis_hourly.py
+++ jobs/03_gold_kpis_hourly.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from config import configuration
-
+from delta.tables import DeltaTable
 
 SILVER_IMPRESSIONS = "s3a://clickstream-event-data/delta/silver/impressions"
 SILVER_CLICKS      = "s3a://clickstream-event-data/delta/silver/clicks"
@@ -31,6 +31,49 @@ def main():
            .getOrCreate()
 
     spark.sparkContext.setLogLevel('WARN')
+
+    def upsert_kpis(microbatch_df, batch_id):
+        spark.sql(f"""
+                CREATE TABLE IF NOT EXISTS delta.`{GOLD_KPIS_PATH}` (
+                  start_window TIMESTAMP,
+                  end_window TIMESTAMP,
+                  campaign_id INT,
+                  ad_id INT,
+                  total_active_users BIGINT,
+                  impressions BIGINT,
+                  clicks BIGINT,
+                  conversions BIGINT,
+                  spend_micros BIGINT,
+                  amount_spent DOUBLE,
+                  total_revenue DOUBLE,
+                  click_thru_rate DOUBLE,
+                  conversion_rate DOUBLE,
+                  revenue_on_ad_spend DOUBLE,
+                  event_date DATE
+                )
+                USING DELTA
+                PARTITIONED BY (event_date)
+                LOCATION '{GOLD_KPIS_PATH}'
+            """)
+
+        target = DeltaTable.forPath(spark, GOLD_KPIS_PATH)
+
+        (
+            target.alias("t")
+            .merge(
+                microbatch_df.alias("s"),
+                """
+                t.start_window = s.start_window AND
+                t.end_window   = s.end_window   AND
+                t.campaign_id  = s.campaign_id  AND
+                t.ad_id        = s.ad_id
+                """
+            )
+            .whenMatchedUpdateAll()
+            .whenNotMatchedInsertAll()
+            .execute()
+        )
+
 
     gold_kpi_impressions = spark.readStream.format('delta').load(SILVER_IMPRESSIONS) \
                       .select(
@@ -74,25 +117,27 @@ def main():
     gold_kpi_base_df = gold_kpi_impressions.unionByName(gold_kpi_clicks).unionByName(gold_kpi_conversions)
     # Calculating KPI's
 
-    gold_kpis_df = gold_kpi_base_df.groupBy(F.window('event_ts','1 hour').alias('window'), 'campaign_id', 'ad_id') \
-                  .agg(
+    gold_kpi_df = gold_kpi_base_df.groupBy(F.window('event_ts', '1 hour').alias('window'), 'campaign_id', 'ad_id') \
+                    .agg(
                         F.sum('impressions').alias('impressions'),
                         F.sum('clicks').alias('clicks'),
                         F.sum('conversions').alias('conversions'),
-                        F.countDistinct('user_id').alias('total_active_users'),
-                        F.sum('cost_micros')/F.lit(1_000_000.0).alias('amount_spent'),
+                        ##F.countDistinct('user_id').alias('total_active_users'),
+                        F.approx_count_distinct('user_id', 0.03).alias('total_active_users'),
+                        F.sum('cost_micros').alias('spend_micros'),
                         F.sum('revenue').alias('total_revenue'),
-                   ) \
-                  .withColumn('click_thru_rate', F.when(F.col('impressions') > 0,
-                                                        F.col('clicks')/F.col('impressions'))
-                                                        .otherwise(F.lit(0.0))) \
-                  .withColumn('conversion_rate',F.when(F.col('clicks')>0,
-                                                       F.col('conversions')/F.col('clicks'))
-                                                       .otherwise(F.lit(0.0))) \
-                  .withColumn('revenue_on_ad_spend', F.when(F.col('amount_spent')>0,
-                                                            F.col('total_revenue')/F.col('amount_spent'))
-                                                            .otherwise(F.lit(0.0))) \
-                  .select(
+                    ) \
+                    .withColumn('amount_spent', F.col('spend_micros')/F.lit(1_000_000.0)) \
+                    .withColumn('click_thru_rate',
+                                F.when(F.col('impressions') >0 , F.col('clicks')/F.col('impressions')).otherwise(F.lit(0.0)),
+                                ) \
+                    .withColumn('conversion_rate',
+                                F.when(F.col('clicks')>0, F.col('conversions')/F.col('clicks')).otherwise(F.lit(0.0)),
+                                ) \
+                    .withColumn('revenue_on_ad_spend',
+                                F.when(F.col('amount_spent')>0, F.col('total_revenue')/F.col('amount_spent')).otherwise(F.lit(0.0)),
+                                ) \
+                    .select(
                         F.col('window.start').alias('start_window'),
                         F.col('window.end').alias('end_window'),
                         'campaign_id',
@@ -101,20 +146,21 @@ def main():
                         'impressions',
                         'clicks',
                         'conversions',
+                        'spend_micros',
                         'amount_spent',
                         'total_revenue',
                         'click_thru_rate',
                         'conversion_rate',
                         'revenue_on_ad_spend',
-                  ) \
-                  .withColumn('event_date', F.to_date('start_window'))
+                    ) \
+                    .withColumn('event_date', F.to_date('start_window'))
 
-    gold_kpis_df.writeStream.format('delta') \
+
+    gold_kpi_df.writeStream.foreachBatch(upsert_kpis) \
                 .outputMode('update') \
                 .option('checkpointLocation', GOLD_CHK_KPIS) \
-                .partitionBy('event_date') \
                 .trigger(processingTime=TRIGGER) \
-                .start(GOLD_KPIS_PATH)
+                .start()
 
     spark.streams.awaitAnyTermination()
 
